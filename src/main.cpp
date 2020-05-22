1fa4049deaec17b6ef07f4247ee761fdc53c1898
leeghwrd <leeghwrd@gmail.com>
2020-05-22 16:46:01 -0700

    Create SDL window
    - Window opens and closes instantly

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 
 int main() {
+  const int SCREEN_WIDTH = 800;
+  const int SCREEN_HEIGHT = 600;
+
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "SDL init failed!" << std::endl;
     return 1;
@@ -10,6 +13,16 @@ int main() {
 
   std::cout << "SDL init suceeded!" << std::endl;
 
+  SDL_Window *window = SDL_CreateWindow(
+      "Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (window == NULL) {
+    SDL_Quit();
+    return 2;
+  }
+
+  SDL_DestroyWindow(window);
   SDL_Quit();
 
   return 0;
