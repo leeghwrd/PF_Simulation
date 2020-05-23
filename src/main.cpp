25061146bfca0e11ddcd9f900a545751392a3a9d
leeghwrd <leeghwrd@gmail.com>
2020-05-23 03:12:01 -0700

    Animate pixels
    - Use sin function to go between 0-255 and set pixel color

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 
 #include <iostream>
-
+#include <cmath>
 #include "Screen.h"
 
 int main() {
@@ -11,18 +11,22 @@ int main() {
     std::cout << "Error initializing SDL." << std::endl;
   }
 
-  bool quit = false;
-
-  while (!quit) {
+  while (true) {
     // Update particles
     
     // draw particles
+    int elapsed = SDL_GetTicks();
+    unsigned char red = (unsigned char)((1 + std::sin(elapsed * 0.001)) * 128);
+    unsigned char green = (unsigned char)((1 + std::sin(elapsed * 0.002)) * 128);
+    unsigned char blue = (unsigned char)((1 + std::sin(elapsed * 0.003)) * 128);
+    
     for (int x = 0; x < lgh::Screen::SCREEN_WIDTH; x++) {
       for (int y = 0; y < lgh::Screen::SCREEN_HEIGHT; y++) {
-        screen.setPixel(x, y, 128, 0, 255);
+        screen.setPixel(x, y, red, green, blue);
       }
     }
 
+    // single white pixel at center of screen
     screen.setPixel(400, 300, 255, 255, 255);
 
     // draw screen
