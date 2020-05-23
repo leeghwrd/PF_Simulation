f4151ca23ac328b34db50e16e04718a255e633de
leeghwrd <leeghwrd@gmail.com>
2020-05-22 17:51:36 -0700

    Add texture, renderer and buffer
    - Use PRESENTVSYNC with renderer to help prevent shearing
    
    - Use RGBA8888 for texture pixel format
    
    - Allocate 32bit for each pixel
    
    - Use memset to test setting pixels to white using hex values

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,44 @@ int main() {
     return 2;
   }
 
+  // create renderer
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+  // create texture
+  SDL_Texture *texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  // check if renderer creation is success
+  if (renderer == NULL) {
+    std::cout << "Could not create renderer" << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 3;
+  }
+
+  // check if texture creation is success
+  if (renderer == NULL) {
+    std::cout << "Could not create texture" << std::endl;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 4;
+  }
+
+  // allocate memory, every pixel requires 32bits
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+  // set all pixels to white using hexadecimal value 0xFF
+  memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+  // setup texture and renderer
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
   bool quit = false;
 
   SDL_Event event;
@@ -37,6 +75,12 @@ int main() {
     }
   }
 
+  // free buffer allocated memory
+  delete[] buffer;
+
+  // destroy SDL resources and quit
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
