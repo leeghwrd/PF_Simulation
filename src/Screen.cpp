ea3c8b290a3c181f550cd5e36f07f8c5f0f8d3fa
leeghwrd <leeghwrd@gmail.com>
2020-05-23 16:10:56 -0700

    Prevent pixels from plotting off the screen
    - Use if statement to check for pixel location

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -54,7 +54,11 @@ bool Screen::init() {
 }
 
 void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
-  
+  // prevent pixels from plotting off the screen
+  if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) {
+    return;
+  }
+
   Uint32 color = 0;
   
   // bit shifting
