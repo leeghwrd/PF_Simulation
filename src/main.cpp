5a1da3bec29862aa73c23a385f1e6976be2eb91e
leeghwrd <leeghwrd@gmail.com>
2020-05-23 16:15:25 -0700

    Add first implemtation of particle swarm

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,45 @@
 #include <SDL2/SDL.h>
 
-#include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
 #include "Screen.h"
+#include "Swarm.h"
 
 int main() {
+
+  std::srand(time(NULL));
+
   lgh::Screen screen;
 
   if (screen.init() == false) {
     std::cout << "Error initializing SDL." << std::endl;
   }
 
+  lgh::Swarm swarm;
+
   while (true) {
     // Update particles
-    
+
     // draw particles
+
     int elapsed = SDL_GetTicks();
     unsigned char red = (unsigned char)((1 + std::sin(elapsed * 0.001)) * 128);
     unsigned char green = (unsigned char)((1 + std::sin(elapsed * 0.002)) * 128);
     unsigned char blue = (unsigned char)((1 + std::sin(elapsed * 0.003)) * 128);
-    
-    for (int x = 0; x < lgh::Screen::SCREEN_WIDTH; x++) {
-      for (int y = 0; y < lgh::Screen::SCREEN_HEIGHT; y++) {
-        screen.setPixel(x, y, red, green, blue);
-      }
-    }
 
-    // single white pixel at center of screen
-    screen.setPixel(400, 300, 255, 255, 255);
+    lgh::Particle *particles = swarm.getParticles();
+
+    for (int i = 0; i < lgh::Swarm::NPARTICLES; i++) {
+      lgh::Particle particle = particles[i];
+      int x = (particle.getX() + 1) * lgh::Screen::SCREEN_WIDTH / 2;
+      int y = (particle.getY() + 1) * lgh::Screen::SCREEN_HEIGHT / 2;
+
+    screen.setPixel(x, y, red, green, blue);
+    
+    }
 
     // draw screen
     screen.update();
