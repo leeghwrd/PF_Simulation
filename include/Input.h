5f99daa347a8a0a060fffb540f8a57bf3cb2d2ed
leeghwrd <leeghwrd@gmail.com>
2020-05-26 14:40:47 -0700

    Add comments for Input.h and Input.cpp

--- include/Input.h
+++ include/Input.h
@@ -5,14 +5,21 @@
 
 namespace lgh {
 
+// Input class definition
+
+// Input handles input from user
 class Input {
 private:
+  // uses SDL for events
   SDL_Event event;
 
 public:
+  // constructor
   Input();
+  // destructor
   ~Input();
 
+  // processEvents method handles user input
   bool processEvents();
 
 };
--- src/Input.cpp
+++ src/Input.cpp
@@ -3,17 +3,24 @@
 
 namespace lgh {
 
+// Input class Implementation
+
+// Constructor
 Input::Input(){}
 
+// Destructor
 Input::~Input(){}
 
+// process events returns false which causes program
+// loop to exit when window is closed by user
 bool Input::processEvents() {
+  // argument event uses reference
   while (SDL_PollEvent(&event)) {
     if (event.type == SDL_QUIT) {
       return false;
     }
   }
-
+  // if user doesnt close window program keeps running
   return true;
 }
 
