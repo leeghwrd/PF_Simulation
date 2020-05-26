5ff0f54432f0ad0a91b9d2ecb5e8d16a546caea2
leeghwrd <leeghwrd@gmail.com>
2020-05-26 14:33:31 -0700

    Add comments for Swarm.h and Swarm.cpp

--- include/Swarm.h
+++ include/Swarm.h
@@ -5,16 +5,30 @@
 
 namespace lgh {
 
+// Swarm class manages a collection of particles
+// Swarm definition
+
 class Swarm {
 private:
+  // collection of particles
   Particle *particles;
+  // calculate time of program running
   int lastTime;
 
 public:
+  // constructor
   Swarm();
+  
+  // destructor
   ~Swarm();
+
+  // calculate particles swarm and update 
   void update(int elapsed);
+  
+  // amount of particles to add to particle swarm
   const static int NPARTICLES = 1000;
+
+  // getParticles returns particles 
   Particle *getParticles();
 };
 
--- src/Swarm.cpp
+++ src/Swarm.cpp
@@ -3,19 +3,27 @@
 
 namespace lgh {
 
+// Swarm class Implementation
+
+// Constructor intializer list with lastTime set to 0 and
+// allocates memory for particles array of the number of particles
 Swarm::Swarm(): lastTime(0), particles(new Particle[NPARTICLES]) {
   std::cout << "Allocating memory for particles" << std::endl;
 }
 
+// Destructor releases memory for particles
 Swarm::~Swarm() {
   std::cout << "Deallocating memory for particles" << std::endl;
   delete [] particles;
 }
 
+// return the amount of particles in swarm
 Particle *Swarm::getParticles() {
   return particles;
 }
 
+// calculate particles with number of particles in swarm
+// NPARTICLES is set to '5000'
 void Swarm::update(int elapsed) {
   
   int interval = elapsed - lastTime;
