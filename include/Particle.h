b86d6b3caf8588c85649d8d6e460dc35968ac8b6
leeghwrd <leeghwrd@gmail.com>
2020-05-26 14:21:03 -0700

    Add comments for Particle.h and Particle.cpp

--- include/Particle.h
+++ include/Particle.h
@@ -3,17 +3,30 @@
 
 namespace lgh {
 
+// Particle Definition  
 class Particle {
 private:
+
+  // x, and y are for particle location
   double x;
   double y;
+
+  // speed and direction handle the movement of particles on screen
   double speed;
   double direction;
+
+  // recalculate location of random particles
   void init();
 
 public:
+  // constructor
   Particle();
+
+  // destructor
   ~Particle();
+
+  // update particles with interval
+  // interval argument as a reference
   void update(int &interval);
 
   // getter / setters
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -5,15 +5,21 @@
 
 namespace lgh {
 
+// Particle Implementation
+
+// Particle Constructor uses intializer list to set x and y coordinates to 0  
 Particle::Particle(): x(0), y(0) {
+
+  // draw particle initial random location
   init();
 }
 
-Particle::~Particle() {
+// Particle Destructor
+Particle::~Particle() {}
 
-}
-
+// calculate speed and direction of particle
 void Particle::init() {
+  // reset x, and y location to 0
   x = 0;
   y = 0;
 
@@ -23,8 +29,10 @@ void Particle::init() {
   speed *= speed;
 }
 
+// update particle location with an interval calculation
 void Particle::update(int &interval) {
-
+  
+  // set a curl affect on particle
   direction += interval * 0.0002;
 
   double xspeed = speed * std::cos(direction);
@@ -34,15 +42,19 @@ void Particle::update(int &interval) {
   x += xspeed * interval;
   y += yspeed * interval;
 
+  // dont allow particles to go off screen, instead redraw
+  // at new random location close to center of screen  
   if (x < -1 || x > 1 || y < -1 || y > 1) {
     init();
   }
 
+  // redraw 1 out of 100 particles randomly 
   if(rand() < RAND_MAX / 100) {
     init();
   }
 }
 
+// getters / setters for x and y coordinates
 void Particle::setX(double x) { x = x; }
 
 double Particle::getX() { return x; }
