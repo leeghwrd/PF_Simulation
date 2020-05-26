29325407a1abefcdd57096bcc0c0e500d8673e43
leeghwrd <leeghwrd@gmail.com>
2020-05-26 15:23:46 -0700

    Add comments for Screen.h and Screen.cpp

--- include/Screen.h
+++ include/Screen.h
@@ -5,25 +5,40 @@
 
 namespace lgh {
 
+// Screen class definition
+
+// Screen class handles creation of the os window
+// it also creates the renderer, texture and buffer
+// to plot pixels on the screen
+
 class Screen {
 public:
+  // value set to not change 800x600 is common size for display
   const static int SCREEN_WIDTH = 800;
   const static int SCREEN_HEIGHT = 600;
 
 private:
+  // SDL member set to pointers
   SDL_Window *window;
   SDL_Renderer *renderer;
   SDL_Texture *texture;
-  
+
+  // buffer1 and buffer2 methods need access to 32 bits
+  // rgba with each being 8 bits * 4 = 32bits
   Uint32 *buffer1;
   Uint32 *buffer2;
 
 public:
+  // constructor
   Screen();
+  // destructor
   ~Screen();
 
+  // update screen
   void update();
+  // set pixel coordinate and color
   void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+  // add box blur effect to entire screen
   void boxBlur();
 
 };
--- src/Screen.cpp
+++ src/Screen.cpp
@@ -3,47 +3,58 @@
 
 namespace lgh {
 
+// Screen class Implementation
+
+// Constructor use initializer list to set values to nullptr's
 Screen::Screen()
     : window(nullptr),
       renderer(nullptr),
       texture(nullptr),
       buffer1(nullptr),
       buffer2(nullptr) {
-        
+  
+  // error initialzing SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cerr << "Failed to initialize SDL!" << std::endl;
     std::cerr << "SDL_Error: " << SDL_GetError() << std::endl;
   }
 
+  // create window with no specific window postion, usually opens at center of screen
+  // width and heiht values are 800x600, Set window title
   window = SDL_CreateWindow("Particle Fire Simulation", SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
+  
+  // quit program if window fails to intialize
   if (window == nullptr) {
     SDL_Quit();
   }
 
-  // create renderer
+  // create renderer and use fallback software rendering
+  // works locally and in udacity workspace without problems
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
-  // create texture
-  texture =
-      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
-
   // check if renderer creation is success
+  // if not destroy window and exit
   if (renderer == nullptr) {
     SDL_DestroyWindow(window);
     SDL_Quit();
   }
 
+  // create texture and use pixel format as byte array
+  texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
   // check if texture creation is success
+  // if not destroy render, window and then exit
   if (texture == nullptr) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
   }
 
+  // sanity check that memory is being allocated
   std::cout << "Allocating memory for buffer1 and buffer2" << std::endl;
 
   // allocate memory, every pixel requires 32bits
@@ -69,6 +80,7 @@ Screen::~Screen() {
   SDL_Quit();
 }
 
+// set pixel coordinate and color using bit shifting operations
 void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
   // prevent pixels from plotting off the screen
   if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) {
@@ -77,7 +89,12 @@ void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
 
   Uint32 color = 0;
 
-  // bit shifting
+  // bit shift color values
+  // results in values that
+  // like: 0xFF003400, 0x123456
+  
+  // each color shifts 8 bits
+  // until the final alpha value
   color += red;
   color <<= 8;
   color += green;
@@ -90,38 +107,53 @@ void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
   buffer1[(y * SCREEN_WIDTH) + x] = color;
 }
 
+// update screen texture and rendering via SDL
 void Screen::update() {
-  // update texture and renderer
   SDL_UpdateTexture(texture, NULL, buffer1, SCREEN_WIDTH * sizeof(Uint32));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
 }
 
+// implementation of box blur algorithm
+// longer the pixel are on screen, more
+// blur affect is added to them
 void Screen::boxBlur() {
+
+  // swap buffers so pixel info is in buffer2
   Uint32 *temp = buffer1;
 
   buffer1 = buffer2;
   buffer2 = temp;
 
+  // iterate over all pixels
   for (int y = 0; y < SCREEN_HEIGHT; y++) {
     for (int x = 0; x < SCREEN_WIDTH; x++) {
       int redTotal = 0;
       int greenTotal = 0;
       int blueTotal = 0;
-
+      
+      // all pixels in entire row
       for (int row = -1; row <= 1; row++) {
+        // all pixels in entire column
         for (int col = -1; col <= 1; col++) {
           int currentX = x + col;
           int currentY = y + row;
 
+          // ignore pixels that arent on the screen
           if (currentX >= 0 && currentX < SCREEN_WIDTH && currentY >= 0 &&
               currentY < SCREEN_HEIGHT) {
+            // access buffer that contains pixel info
             Uint32 color = buffer2[currentY * SCREEN_WIDTH + currentX];
 
-            // shift bits
+            // get buffer2 pixel color value
+            // using bit shifting
+
+            // right shift by 24bit
             Uint8 red = color >> 24;
+            // right shift by 16bit
             Uint8 green = color >> 16;
+            // right shift by 8bit
             Uint8 blue = color >> 8;
 
             redTotal += red;
@@ -131,10 +163,12 @@ void Screen::boxBlur() {
         }
       }
 
+      // set actual color
       Uint8 red = redTotal / 9;
       Uint8 green = greenTotal / 9;
       Uint8 blue = blueTotal / 9;
 
+      // set the final calculated pixel location and color
       setPixel(x, y, red, green, blue);
     }
   }
